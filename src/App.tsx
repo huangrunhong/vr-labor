960ea29ce7f34b0e02eee69d2bd464912899d2b5
Runhong <huang.run.hong.1986@gmail.com>
2024-11-04 12:43:23 +0100

    add vr teleport

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,48 @@
 import { Canvas } from "@react-three/fiber";
 import { Environment, Stats } from "@react-three/drei";
-import { IfSessionModeSupported, XR, createXRStore } from "@react-three/xr";
+import {
+  IfSessionModeSupported,
+  TeleportTarget,
+  XR,
+  XROrigin,
+  createXRStore,
+} from "@react-three/xr";
 
 import Camera from "./components/Camera";
 import Room from "./components/Room";
 
 import "./styles/app.scss";
+import { useState } from "react";
+import { Vector3 } from "three";
 
-const store = createXRStore();
+const store = createXRStore({
+  hand: { teleportPointer: true },
+  controller: { teleportPointer: true },
+});
 
-const App = () => (
-  <main>
-    <IfSessionModeSupported mode="immersive-vr">
-      <div className="vr">
-        <button onClick={() => store.enterVR()}>Enter VR</button>
-      </div>
-    </IfSessionModeSupported>
+const App = () => {
+  const [position, setPosition] = useState(new Vector3(-6, 1.6, -1));
 
-    <Canvas>
-      <XR store={store}>
-        <Environment files="/vr-labor/berlin.hdr" background />
-        <Camera />
-        <Room />
-        <Stats />
-      </XR>
-    </Canvas>
-  </main>
-);
+  return (
+    <main>
+      <IfSessionModeSupported mode="immersive-vr">
+        <div className="vr">
+          <button onClick={() => store.enterVR()}>Enter VR</button>
+        </div>
+      </IfSessionModeSupported>
+      <Canvas>
+        <XR store={store}>
+          <XROrigin position={position} />
+          <Environment files="/vr-labor/berlin.hdr" background />
+          <Camera />
+          <TeleportTarget onTeleport={setPosition}>
+            <Room />
+          </TeleportTarget>
+          <Stats />
+        </XR>
+      </Canvas>
+    </main>
+  );
+};
 
 export default App;
