bdb9d5a8b75c6e8895e59eec49ea76cd0d2d7bcf
Runhong <huang.run.hong.1986@gmail.com>
2024-11-04 11:48:54 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Canvas } from "@react-three/fiber";
+import { Environment, Stats } from "@react-three/drei";
+import { IfSessionModeSupported, XR, createXRStore } from "@react-three/xr";
+
+import Camera from "./components/Camera";
+import Room from "./components/Room";
+
+import "./styles/app.scss";
+
+const store = createXRStore();
+
+const App = () => (
+  <main>
+    <IfSessionModeSupported mode="immersive-vr">
+      <div className="vr">
+        <button onClick={() => store.enterVR()}>Enter VR</button>
+      </div>
+    </IfSessionModeSupported>
+
+    <Canvas>
+      <XR store={store}>
+        <Environment files="/berlin.hdr" background />
+        <Camera />
+        <Room />
+        <Stats />
+      </XR>
+    </Canvas>
+  </main>
+);
+
+export default App;
---
+++ src/components/Room.tsx
@@ -0,0 +1,30 @@
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { AnimationAction, Camera, Object3D, Vector2 } from "three";
+
+const play = (
+  camera: Camera,
+  object?: Object3D,
+  action?: AnimationAction | null
+) => {
+  if (!object || !action) return;
+
+  const stand = new Vector2(camera.position.x, camera.position.z);
+  const target = new Vector2(object.position.x, object.position.z);
+
+  stand.distanceTo(target) < 2 ? action.play() : action.reset();
+};
+
+const Room = () => {
+  const { animations, scene, nodes } = useGLTF("/room.glb");
+  const { actions } = useAnimations(animations, scene);
+  console.log(actions);
+
+  useFrame((state) => {
+    play(state.camera, nodes["Door_moving_lobby"], actions["Door_entrance"]);
+  });
+
+  return <primitive object={scene} />;
+};
+
+export default Room;
