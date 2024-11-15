a3aba9c9025b9f1714c3bed1720ac6b17da575bd
Runhong <huang.run.hong.1986@gmail.com>
2024-11-15 14:55:21 +0100

    try fix local position

--- src/components/Locomotion.tsx
+++ src/components/Locomotion.tsx
@@ -17,11 +17,10 @@ const Locomotion = ({ x, y, z }: LocomotionProps) => {
 
     const x = xr.origin.position.x + translation.x * delta;
     const z = xr.origin.position.z + translation.z * delta;
-    const origin = xr.origin.localToWorld(new Vector3(x, 0, z));
 
     xr.origin.rotation.y += rotation;
 
-    if (collisionDetection(origin, state.scene)) return;
+    if (collisionDetection(new Vector3(x, 0, z), state.scene)) return;
 
     xr.origin.position.x = x;
     xr.origin.position.z = z;
